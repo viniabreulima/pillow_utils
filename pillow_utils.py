92bd5b7a80b099812117557a36f56058240e2d85
Vinicius Lima <eu@viniciuslima.com>
2014-12-01 22:08:57 -0200

    Implemented django_auto_resize_crop_rename. Updated resize_and_crop.

--- pillow_utils.py
+++ pillow_utils.py
@@ -4,11 +4,12 @@
 Collection of Pillow snippets
 '''
 
+import os
 from PIL import Image
 
 class PillowUtils(object):
     @staticmethod
-    def resize_and_crop(img_path, modified_path=None, size=(100,100), crop_type='top', save_params=[]):
+    def resize_and_crop(src_img, dst_path=None, size=(100,100), crop_type='middle', save_params=[]):
         """
         Resize and crop an image to fit the specified size.
 
@@ -25,10 +26,16 @@ class PillowUtils(object):
             ValueError: if an invalid `crop_type` is provided.
         """
         if not crop_type in ('top', 'middle', 'bottom'):
-            raise ValueError('ERROR: invalid value for crop_type')
-            
+            raise ValueError('invalid value for crop_type')
+
+        if isinstance(src_img, Image.Image):
+            img = src_img
+        elif isinstance(src_img, str):
+            img = Image.open(src_img)
+        else:
+            raise ValueError('invalid type for src_img')
         # If height is higher we resize vertically, if not we resize horizontally
-        img = Image.open(img_path)
+
         # Get current and desired ratio for the images
         img_ratio = img.size[0] / float(img.size[1])
         ratio = size[0] / float(size[1])
@@ -67,6 +74,30 @@ class PillowUtils(object):
             img = img.resize((size[0], size[1]),
                     Image.ANTIALIAS)
 
-        if modified_path:
-            img.save(*([modified_path] + save_params))
+        if dst_path:
+            if not os.path.exists(os.path.dirname(dst_path)):
+                os.makedirs(os.path.dirname(dst_path))
+            img.save(*([dst_path] + save_params))
         return img
+
+
+    def django_auto_resize_crop_rename(src_file, dst_prefix=None, size=(100,100),
+                                       crop_type='middle', save_params=[],
+                                       root_dir=''):
+        '''
+        '''
+
+        img_name = src_file.name
+        if img_name.startswith('./'):
+            img_name = img_name[2:]
+        dst_path = '{}@' + 'x'.join((str(x) for x in size)) + '.{}'
+        url = os.path.join(dst_prefix, dst_path.format(*img_name.rsplit('.', 1)))
+        if url.startswith('./'):
+            url = url[2:]
+        dst_path = os.path.join(root_dir, url)
+        PillowUtils.resize_and_crop(src_img=str(src_file.file),
+                                    dst_path=dst_path,
+                                    size=size)
+        return url
+        
+        
